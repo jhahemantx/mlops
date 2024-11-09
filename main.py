b913e51ba717c5f0c0e0e618e82c2cc07ff4c535
maithilguy <maithiliguy@gmail.com>
2024-11-09 18:21:33 +0530

    data validation added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mlProject import logger
 logger.info('welcome to custom log')
 
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 # from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -17,3 +17,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = DataValidationTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
