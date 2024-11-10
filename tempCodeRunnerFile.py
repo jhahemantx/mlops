e7031c92db60b07859dbe866d94c8b8f175060d6
maithilguy <maithiliguy@gmail.com>
2024-11-10 20:57:33 +0530

    flask application created

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,3 @@
-from flask import Flask, render_template, request
-import os
-import numpy as np
-import pandas as pd
-from mlProject.pipeline.prediction import PredictionPipeline
-
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
